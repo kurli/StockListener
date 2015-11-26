f0a4d0c9fa39a586a1a799fcc05007b22c52f23e
kurli <guangzhen@hotmail.com>
2015-11-26 22:09:30 +0800

    Initial Commit

---
+++ StockListener/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StockListener
+//
+//  Created by Guozhen Li on 11/26/15.
+//  Copyright (c) 2015 Guangzhen Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StockListener/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StockListener
+//
+//  Created by Guozhen Li on 11/26/15.
+//  Copyright (c) 2015 Guangzhen Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
